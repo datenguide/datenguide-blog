df4eacc8eb20372b1bc92e4bd9cdc7b9cade64e5
Simon Jockers <hello@simonjockers.com>
2017-12-21 18:34:35 +0100

    Add funder logos to home page

---
+++ src/components/Funders.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logos from '../assets/images/funders.svg'
+
+const Funders = () => (
+  <img src={logos} alt="Gefürdert vom Bundesministerium für Bildung und Forschung" />
+)
+
+export default Funders
--- src/templates/home.js
+++ src/templates/home.js
@@ -5,6 +5,7 @@ import Headline from 'grommet/components/Headline'
 import Markdown from 'grommet/components/Markdown'
 import Header from '../components/Header'
 import Newsletter from '../components/Newsletter'
+import Funders from '../components/Funders'
 import Footer from '../components/Footer'
 
 export default ({ data }) => {
@@ -21,18 +22,14 @@ export default ({ data }) => {
         </Box>
       </Box>
 
-      <Box direction="row" flex="grow" justify="center">
+      <Box direction="row" flex="grow" justify="center" colorIndex="light-2">
         <Box size={{width: {max: 'xxlarge'}}} pad="large" align="start">
-
-          <Split>
-
-            <div dangerouslySetInnerHTML={{ __html: page.html }} />
-
-          </Split>
+          <div dangerouslySetInnerHTML={{ __html: page.html }} />
+          <Funders />
         </Box>
       </Box>
 
-      <Box direction="row" flex="grow" justify="center" colorIndex="light-2">
+      <Box direction="row" flex="grow" justify="center">
         <Newsletter />
       </Box>
 
