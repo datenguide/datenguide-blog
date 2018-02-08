ed6a568839a3e4573008856ae9b733d209fa03ba
Simon Jockers <hello@simonjockers.com>
2018-02-08 20:16:56 +0100

    Use RMWC instead of Grommet layout components in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,15 @@
 import React from 'react'
-import Box from 'grommet/components/Box'
-import Footer from 'grommet/components/Footer'
 import GatsbyLink from 'gatsby-link'
+import { Grid, GridCell } from 'rmwc/Grid'
 
 export default function AppFooter(props) {
   return (
-    <Footer justify="center" colorIndex="neutral-2-a">
-      <Box
-        size={{ width: { max: 'xxlarge' } }}
-        direction="row"
-        responsive={false}
-        justify="start"
-        align="center"
-        pad={{ horizontal: 'medium', vertical: 'small' }}
-        flex="grow"
-      >
-        <Box flex="grow" align="end">
+    <footer className="footer">
+      <Grid>
+        <GridCell span="12">
           <GatsbyLink to="contact">Kontakt und Impressum</GatsbyLink>
-        </Box>
-      </Box>
-    </Footer>
+        </GridCell>
+      </Grid>
+    </footer>
   )
 }
