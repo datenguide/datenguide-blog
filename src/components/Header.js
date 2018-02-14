ab10df65dc6a4b0cb5d585ea8aa5ac6d046e9fb9
Simon Jockers <hello@simonjockers.com>
2018-02-14 19:01:28 +0100

    Toolbar: Fix to viewport

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import {
-  Toolbar,
-  ToolbarRow,
-  ToolbarSection,
-  ToolbarTitle,
-  ToolbarMenuIcon,
-  ToolbarIcon
-} from 'rmwc/Toolbar'
+import { Toolbar, ToolbarRow, ToolbarSection } from 'rmwc/Toolbar'
 
 import Logo from './Logo'
 
 export default function AppHeader(props) {
   return (
-    <Toolbar>
+    <Toolbar fixed waterfall>
       <ToolbarRow>
         <ToolbarSection alignStart>
           <Logo />
