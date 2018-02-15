0e8c1da87b9e01a7398e8c8fc5465fda2f040fe7
Simon Jockers <hello@simonjockers.com>
2018-02-15 16:04:03 +0100

    Add basic grid layout for district pages

--- src/templates/district.js
+++ src/templates/district.js
@@ -1,32 +1,44 @@
 import React from 'react'
+import { Grid, GridCell } from 'rmwc/Grid'
+import Header from '../components/Header'
 
 export default ({ data }) => {
   const { district } = data
 
   return (
-    <div>
-      <h1>{district.name}</h1>
+    <div className="district">
+      <Header />
 
-      <ul>
-        <li>{district.name_ext}</li>
-        <li>
-          {district.pop.m} / {district.pop.w}
-        </li>
-      </ul>
+      <Grid>
+        <GridCell span="8">
+          <h1>{district.name}</h1>
 
-      <ul>
-        <li>{district.Schulstatistik.Gymnasien.BIL003.BILKL2.JGSTUFE11}</li>
-        <li>{district.Schulstatistik.Gymnasien.BIL003.BILKL2.JGSTUFE7}</li>
-      </ul>
+          <ul>
+            <li>{district.name_ext}</li>
+            <li>
+              {district.pop.m} / {district.pop.w}
+            </li>
+          </ul>
+        </GridCell>
+      </Grid>
 
-      <ul>
-        <li>{district.Schulstatistik.Gymnasien.BIL003.GES.I}</li>
-        <li>{district.Schulstatistik.Gymnasien.BIL003.GES.M}</li>
-      </ul>
+      <Grid>
+        <GridCell span="8">
+          <ul>
+            <li>{district.Schulstatistik.Gymnasien.BIL003.BILKL2.JGSTUFE11}</li>
+            <li>{district.Schulstatistik.Gymnasien.BIL003.BILKL2.JGSTUFE7}</li>
+          </ul>
 
-      <ul>
-        <li>{district.Schulstatistik.Gymnasien.BIL003.NAT.NATA}</li>
-      </ul>
+          <ul>
+            <li>{district.Schulstatistik.Gymnasien.BIL003.GES.I}</li>
+            <li>{district.Schulstatistik.Gymnasien.BIL003.GES.M}</li>
+          </ul>
+
+          <ul>
+            <li>{district.Schulstatistik.Gymnasien.BIL003.NAT.NATA}</li>
+          </ul>
+        </GridCell>
+      </Grid>
     </div>
   )
 }
