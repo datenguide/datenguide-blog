df3c70893646aa3d991af2f9397af2cfbae0b9a5
Simon Jockers <hello@simonjockers.com>
2018-02-20 21:44:46 +0100

    Add basic styling for footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import GatsbyLink from 'gatsby-link'
 import { Grid, GridCell } from 'rmwc/Grid'
+import GithubCircleIcon from 'mdi-react/GithubCircleIcon'
 
 export default function AppFooter(props) {
   return (
     <footer className="footer">
       <Grid>
-        <GridCell span="12">
-          <GatsbyLink to="contact">Kontakt und Impressum</GatsbyLink>
+        <GridCell span="8">
+          <a
+            className="footer__link footer__link--github"
+            href="http://github.com/datenguide"
+          >
+            <GithubCircleIcon />
+            Datenguide ist open source
+          </a>
+        </GridCell>
+        <GridCell span="4">
+          <GatsbyLink
+            className="footer__link footer__link--contact"
+            to="contact"
+          >
+            Kontakt und Impressum
+          </GatsbyLink>
         </GridCell>
       </Grid>
     </footer>
