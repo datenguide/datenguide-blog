9eab0cf54f84fad10e3d9aa2f206de27a831a7b5
Simon Jockers <hello@simonjockers.com>
2018-02-23 10:37:44 +0100

    Categories: Add dummy categories to district page

--- src/templates/district.js
+++ src/templates/district.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Grid, GridCell } from 'rmwc/Grid'
+import { Button } from 'rmwc/Button'
 import { VictoryBar, VictoryChart } from 'victory'
 
 import Header from '../components/Header'
 import DistrictHeader from '../components/district/DistrictHeader.js'
+import Footer from '../components/Footer'
 
 export default ({ data }) => {
   const { district } = data
@@ -12,19 +14,93 @@ export default ({ data }) => {
     <div className="district">
       <Header />
       <DistrictHeader district={district} />
-
       <Grid>
-        <GridCell span="8">
+        <GridCell span="7">
+          <h3>Arbeit und Soziales</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <Button stroked href="./arbeit">
+            Mehr Daten zum Thema Arbeit
+          </Button>
+        </GridCell>
+        <GridCell span="5">
           <VictoryChart domainPadding={50}>
             <VictoryBar
               data={[
-                { x: 'male', y: district.BEVSTD.GESM },
-                { x: 'female', y: district.BEVSTD.GESW }
+                { x: 'A', y: district.BEVSTD.GESM },
+                { x: 'B', y: district.BEVSTD.GESW }
               ]}
             />
           </VictoryChart>
         </GridCell>
       </Grid>
+      <hr />
+      <Grid>
+        <GridCell span="7">
+          <h3>Bildung</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <Button stroked href="./bildung">
+            Mehr Daten zum Thema Bildung
+          </Button>
+        </GridCell>
+        <GridCell span="5">
+          <VictoryChart domainPadding={50}>
+            <VictoryBar
+              data={[
+                { x: 'A', y: district.BEVSTD.GESM },
+                { x: 'B', y: district.BEVSTD.GESW }
+              ]}
+            />
+          </VictoryChart>
+        </GridCell>
+      </Grid>
+      <hr />
+      <Grid>
+        <GridCell span="7">
+          <h3>Gesundheit</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <Button stroked href="./gesundheit">
+            Mehr Daten zum Thema Gesundheit
+          </Button>
+        </GridCell>
+        <GridCell span="5">
+          <VictoryChart domainPadding={50}>
+            <VictoryBar
+              data={[
+                { x: 'A', y: district.BEVSTD.GESM },
+                { x: 'B', y: district.BEVSTD.GESW }
+              ]}
+            />
+          </VictoryChart>
+        </GridCell>
+      </Grid>
+      <Footer />
     </div>
   )
 }
