45df1709feeea12bfda7ae29f054a4cf40320e25
Simon Jockers <hello@simonjockers.com>
2018-02-23 10:02:52 +0100

    Categories: Add dummy content to pages

--- src/templates/category.js
+++ src/templates/category.js
@@ -5,17 +5,86 @@ import { VictoryBar, VictoryChart } from 'victory'
 import Header from '../components/Header'
 import DistrictHeader from '../components/district/DistrictHeader.js'
 
-export default ({ data }) => {
+export default ({ data, pathContext }) => {
   const { district } = data
 
   return (
     <div className="district">
       <Header />
 
-      <h1>CATEGORY</h1>
-
       <Grid>
-        <GridCell span="8">
+        <GridCell span="12">
+          <h1>
+            {district.name.toUpperCase()}&nbsp;/&nbsp;
+            {pathContext.category.toUpperCase()}
+          </h1>
+        </GridCell>
+        <GridCell span="7">
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+        </GridCell>
+        <GridCell span="5">
+          <VictoryChart domainPadding={50}>
+            <VictoryBar
+              data={[
+                { x: 'male', y: district.BEVSTD.GESM },
+                { x: 'female', y: district.BEVSTD.GESW }
+              ]}
+            />
+          </VictoryChart>
+        </GridCell>
+      </Grid>
+      <hr />
+      <Grid>
+        <GridCell span="5">
+          <VictoryChart domainPadding={50}>
+            <VictoryBar
+              data={[
+                { x: 'male', y: district.BEVSTD.GESM },
+                { x: 'female', y: district.BEVSTD.GESW }
+              ]}
+            />
+          </VictoryChart>
+        </GridCell>
+        <GridCell span="7">
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+        </GridCell>
+      </Grid>
+      <hr />
+      <Grid>
+        <GridCell span="7">
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+        </GridCell>
+        <GridCell span="5">
           <VictoryChart domainPadding={50}>
             <VictoryBar
               data={[
