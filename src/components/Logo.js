5e45db6142bb32baccc93c60f4de4c5be880f9ba
Simon Jockers <hello@simonjockers.com>
2018-02-24 20:36:15 +0100

    Toolbar: Link logo to home page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,3 +1,8 @@
 import React from 'react'
+import GatsbyLink from 'gatsby-link'
 
-export default () => <h1 className="logo">datenguide</h1>
+export default () => (
+  <GatsbyLink to="/" className="logo">
+    <h1>datenguide</h1>
+  </GatsbyLink>
+)
