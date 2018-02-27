78a6e4e036c73122d1a569568c3f56275a2d847c
Simon Jockers <hello@simonjockers.com>
2018-02-27 23:31:50 +0100

    Region: Add basic chart for BEVSTD over time

--- src/components/region/RegionMeta.js
+++ src/components/region/RegionMeta.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { Grid, GridCell } from 'rmwc/Grid'
-import { VictoryPie } from 'victory'
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory'
 import VectorSquareIcon from 'mdi-react/VectorSquareIcon'
 import MapMarkerIcon from 'mdi-react/MapMarkerIcon'
 import MapMarkerMultipleIcon from 'mdi-react/MapMarkerMultipleIcon'
+import _ from 'lodash'
 
 import theme from '../../components/theme'
 
 export default function RegionMeta({ region }) {
+  const popData = _(region.BEVSTD.ALTX20.INSGESAMT.GEST__years)
+    .mapValues((value, id) => ({
+      x: id,
+      y: parseInt(value, 10)
+    }))
+    .values() //get the values of the result
+    .sortBy(value => value.x)
+    .value() //unwrap array of objects
+
+  console.log(popData)
+
   return (
     <div className="region-meta">
       <Grid>
         <GridCell span="8">
+          <VictoryChart domainPadding={50} theme={theme}>
+            <VictoryBar data={popData} />
+          </VictoryChart>
           <div className="region-meta__demo">{region.BEVSTD.GEST}</div>
           <VictoryPie
             innerRadius={100}
