63e0ac0842cd063bcd8dc77dcdf69fb02a15b4c4
Simon Jockers <hello@simonjockers.com>
2018-03-09 16:34:14 +0100

    RegionMeta: Add explicit axis formatting

--- src/components/region/Chart.js
+++ src/components/region/Chart.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { VictoryChart, VictoryBar, VictoryTheme } from 'victory'
+import { VictoryChart, VictoryBar, VictoryAxis, VictoryTheme } from 'victory'
 
 import theme from '../../components/theme'
 
 export default function RegionMeta({ data }) {
   const popData = _(data)
     .mapValues((value, id) => ({
-      x: +id.substring(1),
+      x: id.substring(1),
       y: parseInt(value, 10)
     }))
     .values()
@@ -19,6 +19,11 @@ export default function RegionMeta({ data }) {
       padding={{ top: 20, bottom: 40, left: 60, right: 40 }}
     >
       <VictoryBar data={popData} />
+      <VictoryAxis fixLabelOverlap />
+      <VictoryAxis
+        dependentAxis
+        tickFormat={val => Intl.NumberFormat('de').format(val)}
+      />
     </VictoryChart>
   )
 }
