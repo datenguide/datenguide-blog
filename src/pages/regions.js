74af0ae972c1e666c13367a78c649ac67e19fd60
Simon Jockers <hello@simonjockers.com>
2018-03-22 12:06:36 +0100

    Regions: List districts by federal state

--- src/pages/regions.js
+++ src/pages/regions.js
@@ -1,26 +1,60 @@
 import React from 'react'
+import GatsbyLink from 'gatsby-link'
 
-const renderRegion = ({ node }) => {
-  return (
-    <li key={node.id}>
-      <a href={node.slug}>{node.name}</a>
-    </li>
-  )
+const DistrictList = ({ state }) => (
+  <ul>
+    {state.districts.map(district => (
+      <li key={district.id}>
+        <GatsbyLink to={district.slug}>{district.name}</GatsbyLink>
+      </li>
+    ))}
+  </ul>
+)
+
+const prepareData = ({ regions, stateNames }) => {
+  const districts = regions.edges
+    .filter(({ node }) => node.slug !== node.state.slug)
+    .map(({ node }) => node)
+
+  const states = regions.edges
+    .filter(({ node }) => node.slug === node.state.slug)
+    .map(({ node }) => node)
+    .reduce((map, state) => ((map[state.slug] = state), map), {})
+
+  return stateNames.distinct.map(stateSlug => ({
+    ...states[stateSlug],
+    districts: districts.filter(({ state }) => stateSlug === state.slug)
+  }))
 }
 
-export default ({ data }) => {
-  const regions = data.allRegion.edges
-  return <ul>{regions.map(renderRegion)}</ul>
-}
+export default ({ data }) => (
+  <ul>
+    {prepareData(data).map(state => (
+      <li key={state.id}>
+        <h3>{state.name}</h3>
+        <DistrictList state={state} />
+      </li>
+    ))}
+  </ul>
+)
 
 export const query = graphql`
   query regionsOverview {
-    allRegion {
+    stateNames: allRegion(filter: { slug: { ne: "deutschland" } }) {
+      distinct(field: state___slug)
+    }
+    regions: allRegion(
+      filter: { slug: { ne: null } }
+      sort: { fields: [slug], order: ASC }
+    ) {
       edges {
         node {
           id
           slug
           name
+          state {
+            slug
+          }
         }
       }
     }
