abec9bce2dbdde655f7d0dc0e700351782ebfa34
Simon Jockers <hello@simonjockers.com>
2018-03-22 18:07:26 +0100

    Regions: Apply multi column layout to district list

--- src/components/DistrictList.js
+++ src/components/DistrictList.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import GatsbyLink from 'gatsby-link'
 
+import '../scss/components/district-list.scss'
+
 export default function DistrictList({ districts }) {
   return (
     <ul className="district-list">
       {districts.map(district => (
-        <li key={district.id}>
-          <GatsbyLink to={district.slug}>{district.name}</GatsbyLink>
+        <li className="district-list__item" key={district.id}>
+          <GatsbyLink className="district-list__link" to={district.slug}>
+            {district.name}
+          </GatsbyLink>
         </li>
       ))}
     </ul>
