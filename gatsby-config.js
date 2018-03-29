66a2670b3fb9c23d9e275607e8d7e10bc9f8c93a
Simon Jockers <hello@simonjockers.com>
2018-03-29 18:16:55 +0200

    Update `gatsby-source-datenguide` to 1.0.3

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     title: 'Datenguide'
   },
   plugins: [
+    'gatsby-transformer-remark',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -17,10 +18,63 @@ module.exports = {
     {
       resolve: 'gatsby-source-datenguide',
       options: {
-        queryUrl: 'http://127.0.0.1:5000/?query=',
-        obtainQueryUrl: 'http://127.0.0.1:5000/query/'
+        queryUrl: 'https://api.datengui.de/?query=',
+        query: `
+          {
+            regions {
+              id
+              name
+              name_ext
+              slug
+              state {
+                id
+                name
+                slug
+              }
+              geo {
+                lat
+                lon
+                bbox
+              }
+              FLC006
+              BEVSTD {
+                GESM
+                GESW
+                GEST
+              }
+              BEVSTD {
+                ALTX20 {
+                  INSGESAMT {
+                    GEST__years {
+                      _1995
+                      _1996
+                      _2004
+                      _2014
+                      _2003
+                      _1998
+                      _1999
+                      _2000
+                      _2008
+                      _2001
+                      _2012
+                      _2011
+                      _2015
+                      _2013
+                      _2002
+                      _2005
+                      _1997
+                      _2010
+                      _2007
+                      _2009
+                      _2006
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `
       }
-    },
-    'gatsby-transformer-remark'
+    }
   ]
 }
