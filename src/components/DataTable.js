10e212e7865f901433a8a31b4fc6ff5a6bd32d0d
Simon Jockers <hello@simonjockers.com>
2018-04-19 21:02:28 +0200

    DataTable: Transpose table content

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,20 +2,41 @@ import React from 'react'
 
 import '../scss/components/data-table.scss'
 
+const VerticalTable = ({ columnLabels, data }) => (
+  <table>
+    <tbody>
+      <tr>
+        <th>{columnLabels[0]}</th>
+        {data.map(row => <td>{row.x}</td>)}
+      </tr>
+      <tr>
+        <th>{columnLabels[1]}</th>
+        {data.map(row => <td>{row.y}</td>)}
+      </tr>
+    </tbody>
+  </table>
+)
+
+const HorizontalTable = ({ columnLabels, data }) => (
+  <table>
+    <thead>
+      <tr>{columnLabels.map(label => <th>{label}</th>)}</tr>
+    </thead>
+    <tbody>
+      {data.map(row => (
+        <tr>
+          <td>{row.x}</td>
+          <td>{row.y}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
+
 export default function DataTable({ columnLabels, data }) {
   return (
-    <table className="data-table">
-      <thead>
-        <tr>{columnLabels.map(label => <th>{label}</th>)}</tr>
-      </thead>
-      <tbody>
-        {data.map(row => (
-          <tr>
-            <td>{row.x}</td>
-            <td>{row.y}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="data-table">
+      <VerticalTable columnLabels={columnLabels} data={data} />
+    </div>
   )
 }
