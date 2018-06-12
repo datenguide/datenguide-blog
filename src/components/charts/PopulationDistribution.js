94f3ae604c3b424d9c06ca70a537f15747da88f9
Simon Jockers <hello@simonjockers.com>
2018-06-12 15:52:02 +0200

    Population distribution: Make population groups explicit

--- src/components/charts/PopulationDistribution.js
+++ src/components/charts/PopulationDistribution.js
@@ -12,24 +12,87 @@ import ChartContainer from '../ChartContainer'
 import theme from '../theme'
 import query from 'raw-loader!./PopulationOverTime.graphql'
 
-const PopulationOverTime = ({ data }) => {
-  const data1915 = _(data.BEVSTD.ALTX20)
-    .mapValues((value, id) => ({
-      x: id,
-      y: parseInt(value.GEST__years._1995, 10)
-    }))
-    .values()
-    .sortBy(value => value.x)
-    .value()
+const popGroups = [
+  {
+    key: 'ALT003B06',
+    label: '03 bis 06'
+  },
+  {
+    key: 'ALT006B10',
+    label: '06 bis 10'
+  },
+  {
+    key: 'ALT010B15',
+    label: '10 bis 15'
+  },
+  {
+    key: 'ALT015B18',
+    label: '15 bis 18'
+  },
+  {
+    key: 'ALT018B20',
+    label: '18 bis 20'
+  },
+  {
+    key: 'ALT020B25',
+    label: '20 bis 25'
+  },
+  {
+    key: 'ALT025B30',
+    label: '25 bis 30'
+  },
+  {
+    key: 'ALT030B35',
+    label: '30 bis 35'
+  },
+  {
+    key: 'ALT035B40',
+    label: '35 bis 40'
+  },
+  {
+    key: 'ALT040B45',
+    label: '40 bis 45'
+  },
+  {
+    key: 'ALT045B50',
+    label: '45 bis 50'
+  },
+  {
+    key: 'ALT050B55',
+    label: '50 bis 55'
+  },
+  {
+    key: 'ALT055B60',
+    label: '55 bis 60'
+  },
+  {
+    key: 'ALT060B65',
+    label: '60 bis 65'
+  },
+  {
+    key: 'ALT065B75',
+    label: '65 bis 75'
+  },
+  {
+    key: 'ALT075UM',
+    label: 'über 75'
+  }
+]
 
-  const data2015 = _(data.BEVSTD.ALTX20)
-    .mapValues((value, id) => ({
-      x: id,
-      y: parseInt(value.GEST__years._2015, 10)
-    }))
-    .values()
-    .sortBy(value => value.x)
-    .value()
+const PopulationOverTime = ({ data }) => {
+  const popData = data.BEVSTD.ALTX20
+
+  const data1915 = popGroups.map(({ key, label }) => ({
+    x: label,
+    y: parseInt(popData[key].GEST__years._1995)
+  }))
+
+  const data2015 = popGroups.map(({ key, label }) => ({
+    x: label,
+    y: parseInt(popData[key].GEST__years._2015)
+  }))
+
+  debugger
 
   return (
     <ChartContainer query={query} data={data1915}>
