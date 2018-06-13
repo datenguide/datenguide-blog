7f6b78a8db0d22d60d53196a211ebc08d3593887
Simon Jockers <hello@simonjockers.com>
2018-06-13 14:53:39 +0200

    Population distribution: Scale x axis accoring to age groups

--- src/components/charts/PopulationDistribution.js
+++ src/components/charts/PopulationDistribution.js
@@ -14,63 +14,64 @@ import ChartContainer from '../ChartContainer'
 import theme from '../theme'
 import query from 'raw-loader!./PopulationOverTime.graphql'
 
-const popGroups = [
-  { key: 'ALT003B06', label: '03 bis 06' },
-  { key: 'ALT006B10', label: '06 bis 10' },
-  { key: 'ALT010B15', label: '10 bis 15' },
-  { key: 'ALT015B18', label: '15 bis 18' },
-  { key: 'ALT018B20', label: '18 bis 20' },
-  { key: 'ALT020B25', label: '20 bis 25' },
-  { key: 'ALT025B30', label: '25 bis 30' },
-  { key: 'ALT030B35', label: '30 bis 35' },
-  { key: 'ALT035B40', label: '35 bis 40' },
-  { key: 'ALT040B45', label: '40 bis 45' },
-  { key: 'ALT045B50', label: '45 bis 50' },
-  { key: 'ALT050B55', label: '50 bis 55' },
-  { key: 'ALT055B60', label: '55 bis 60' },
-  { key: 'ALT060B65', label: '60 bis 65' },
-  { key: 'ALT065B75', label: '65 bis 75' },
-  { key: 'ALT075UM', label: 'über 75' }
+const ageGroup = [
+  { key: 'ALT000B03', min: 0, max: 3, desc: 'unter 3 Jahre' },
+  { key: 'ALT003B06', min: 3, max: 6, desc: '3 bis 6 Jahre' },
+  { key: 'ALT006B10', min: 6, max: 10, desc: '6 bis 10 Jahre' },
+  { key: 'ALT010B15', min: 10, max: 15, desc: '10 bis 15 Jahre' },
+  { key: 'ALT015B18', min: 15, max: 18, desc: '15 bis 18 Jahre' },
+  { key: 'ALT018B20', min: 18, max: 20, desc: '18 bis 20 Jahre' },
+  { key: 'ALT020B25', min: 20, max: 25, desc: '20 bis 25 Jahre' },
+  { key: 'ALT025B30', min: 25, max: 30, desc: '25 bis 30 Jahre' },
+  { key: 'ALT030B35', min: 30, max: 35, desc: '30 bis 35 Jahre' },
+  { key: 'ALT035B40', min: 35, max: 40, desc: '35 bis 40 Jahre' },
+  { key: 'ALT040B45', min: 40, max: 45, desc: '40 bis 45 Jahre' },
+  { key: 'ALT045B50', min: 45, max: 50, desc: '45 bis 50 Jahre' },
+  { key: 'ALT050B55', min: 50, max: 55, desc: '50 bis 55 Jahre' },
+  { key: 'ALT055B60', min: 55, max: 60, desc: '55 bis 60 Jahre' },
+  { key: 'ALT060B65', min: 60, max: 65, desc: '60 bis 65 Jahre' },
+  { key: 'ALT065B75', min: 65, max: 75, desc: '65 bis 75 Jahre' },
+  { key: 'ALT075UM', min: 75, max: 90, desc: 'über 75 Jahre' }
 ]
+
+const mapPerYear = ({ key, min, max, desc }, year, popData) => ({
+  x: (max - min) / 2 + min,
+  y: parseInt(popData[key].GEST__years[`_${year}`]),
+  year,
+  min,
+  max,
+  desc
+})
 
 const PopulationOverTime = ({ data }) => {
   const popData = data.BEVSTD.ALTX20
-
-  const data1915 = popGroups.map(({ key, label }) => ({
-    x: label,
-    y: parseInt(popData[key].GEST__years._1995),
-    year: 1995
-  }))
-
-  const data2015 = popGroups.map(({ key, label }) => ({
-    x: label,
-    y: parseInt(popData[key].GEST__years._2015),
-    year: 2005
-  }))
-
+  const data1995 = ageGroup.map(d => mapPerYear(d, 1995, popData))
+  const data2015 = ageGroup.map(d => mapPerYear(d, 2015, popData))
   const numberFormat = Intl.NumberFormat('de').format
+  const xMax = Math.max(...ageGroup.map(d => d.max))
+  const domain = { x: [0, xMax], y: [0, 100000] }
+  const tooltip = d => `${d.desc}:\n${numberFormat(d.y)} (${d.year})`
 
   return (
-    <ChartContainer query={query} data={data1915}>
+    <ChartContainer query={query} data={data1995}>
       <VictoryChart
         theme={theme}
         padding={{ top: 30, bottom: 40, left: 60, right: 40 }}
         containerComponent={
           <VictoryVoronoiContainer
-            labels={d => `${d.x}:\n${numberFormat(d.y)} (${d.year})`}
-            labelComponent={<VictoryTooltip />}
+            labels={tooltip}
+            labelComponent={<VictoryTooltip dy={2} />}
           />
         }
       >
         <VictoryArea
           interpolation="step"
-          data={data1915}
-          labelComponent={<VictoryTooltip />}
+          data={data1995}
           style={{ data: { fill: '#dadada' } }}
         />
         <VictoryLine interpolation="step" data={data2015} />
         <VictoryAxis fixLabelOverlap />
-        <VictoryAxis dependentAxis tickFormat={d => numberFormat(d)} />
+        <VictoryAxis dependentAxis tickFormat={numberFormat} />
       </VictoryChart>
     </ChartContainer>
   )
