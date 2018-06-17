eb5d8d2b10900b5682845978c814fb5a33bfe51e
Simon Jockers <hello@simonjockers.com>
2018-06-17 21:09:34 +0200

    Region meta: Remove redundant population info

--- src/components/region/RegionMeta.js
+++ src/components/region/RegionMeta.js
@@ -32,22 +32,6 @@ export default function RegionMeta({ region, meta }) {
 
           <h3>Bevölkerungsverteilung</h3>
           <PopulationDistribution data={region} />
-
-          <div className="region-meta__demo">
-            {region.BEVSTD.GEST} Einwohner
-            <div className="region-meta__donut">
-              <VictoryPie
-                innerRadius={40}
-                width={150}
-                height={150}
-                theme={theme}
-                data={[
-                  { x: 'M', y: +region.BEVSTD.GESM },
-                  { x: 'W', y: +region.BEVSTD.GESW }
-                ]}
-              />
-            </div>
-          </div>
         </GridCell>
         <GridCell span="4">
           <div className="region-meta__listing">
@@ -70,37 +54,3 @@ export default function RegionMeta({ region, meta }) {
     </div>
   )
 }
-
-export const query = graphql`
-  fragment population on Region {
-    BEVSTD {
-      ALTX20 {
-        INSGESAMT {
-          GEST__years {
-            _1995
-            _1996
-            _1997
-            _1998
-            _1999
-            _2000
-            _2001
-            _2002
-            _2003
-            _2004
-            _2005
-            _2006
-            _2007
-            _2008
-            _2009
-            _2010
-            _2011
-            _2012
-            _2013
-            _2014
-            _2015
-          }
-        }
-      }
-    }
-  }
-`
