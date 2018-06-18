11e067b68e1517e62fd8fde416feeae449d58f78
Simon Jockers <hello@simonjockers.com>
2018-06-18 18:54:33 +0200

    Region meta: Add additional region info

--- src/components/region/RegionMeta.js
+++ src/components/region/RegionMeta.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Grid, GridCell } from 'rmwc/Grid'
 import { VictoryPie, VictoryChart, VictoryTheme } from 'victory'
 import VectorSquareIcon from 'mdi-react/VectorSquareIcon'
-import MapMarkerIcon from 'mdi-react/MapMarkerIcon'
 import MapMarkerMultipleIcon from 'mdi-react/MapMarkerMultipleIcon'
+import MapMarkerIcon from 'mdi-react/MapMarkerIcon'
+import GenderMaleFemaleIcon from 'mdi-react/GenderMaleFemaleIcon'
+import AccountMultipleIcon from 'mdi-react/AccountMultipleIcon'
 
 import theme from '../../components/theme'
 import PopulationOverTime from '../charts/PopulationOverTime'
 import PopulationDistribution from '../charts/PopulationDistribution'
 
 import '../../scss/components/region-meta.scss'
+
+const numberFormat = Intl.NumberFormat('de').format
 
 export default function RegionMeta({ region, meta }) {
   return (
@@ -33,15 +37,40 @@ export default function RegionMeta({ region, meta }) {
           <h3>Bevölkerungsverteilung</h3>
           <PopulationDistribution data={region} />
         </GridCell>
+
         <GridCell span="4">
           <div className="region-meta__listing">
             <h3>
               {region.name_ext} {region.name}
             </h3>
+
+            <div className="region-meta__highlight">
+              <b>{numberFormat(region.BEVSTD.GEST)}</b>
+              Einwohner (2017)
+            </div>
             <ul>
+              <li>
+                <GenderMaleFemaleIcon />
+                {numberFormat(
+                  Math.round((100 / region.BEVSTD.GEST) * region.BEVSTD.GESW)
+                )}
+                % weiblich
+                {' / '}
+                {numberFormat(
+                  Math.round((100 / region.BEVSTD.GEST) * region.BEVSTD.GESM)
+                )}
+                % männlich
+              </li>
               <li>
                 <VectorSquareIcon />
                 {region.FLC006} km² Fläche
+              </li>
+              <li>
+                <AccountMultipleIcon />
+                {numberFormat(
+                  Math.round(+region.BEVSTD.GEST / +region.FLC006)
+                )}{' '}
+                Einwohner pro km²
               </li>
               <li>
                 <MapMarkerMultipleIcon />
