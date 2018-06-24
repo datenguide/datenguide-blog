9f11f9445e273f48476ea660d87b668b66e9af8c
Simon Jockers <hello@simonjockers.com>
2018-06-24 10:37:47 +0200

    Header: Use `TopAppBar` component for better scrolling behavior

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import GatsbyLink from 'gatsby-link'
-import { Toolbar, ToolbarRow, ToolbarSection } from 'rmwc/Toolbar'
+import { TopAppBar, TopAppBarRow, TopAppBarSection } from 'rmwc/TopAppBar'
 
 import Logo from './Logo'
 import '../scss/components/_header.scss'
 
 export default function AppHeader(props) {
   return (
-    <Toolbar fixed waterfall>
-      <ToolbarRow>
-        <ToolbarSection alignStart>
+    <TopAppBar className="header" dense>
+      <TopAppBarRow className="header__row">
+        <TopAppBarSection alignStart>
           <Logo />
-        </ToolbarSection>
-        <ToolbarSection alignEnd>
-          <GatsbyLink to="/regions">Explore</GatsbyLink>
-        </ToolbarSection>
-      </ToolbarRow>
-    </Toolbar>
+        </TopAppBarSection>
+        <TopAppBarSection alignEnd>
+          <GatsbyLink className="header__nav-item" to="/regions">
+            Alle Regionen
+          </GatsbyLink>
+        </TopAppBarSection>
+      </TopAppBarRow>
+    </TopAppBar>
   )
 }
