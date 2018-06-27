cf4d489204963dec692652fb77b40dc8196c16f8
Simon Jockers <hello@simonjockers.com>
2018-06-27 23:14:22 +0200

    Population chart: Add series labels

--- src/components/charts/PopulationDensity.js
+++ src/components/charts/PopulationDensity.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
-import { VictoryChart, VictoryArea, VictoryLine, VictoryAxis } from 'victory'
+import {
+  VictoryLabel,
+  VictoryChart,
+  VictoryArea,
+  VictoryLine,
+  VictoryAxis
+} from 'victory'
 import _ from 'lodash'
 
 import ChartContainer from '../ChartContainer'
@@ -17,7 +23,7 @@ const prepareData = ({ BEVSTD, FLC006 }) => {
 
   return _(populationPerYear)
     .mapValues((value, id) => ({
-      x: id.substring(1),
+      x: +id.substring(1),
       y: Math.round(value / area)
     }))
     .values()
@@ -41,16 +47,32 @@ const PopulationDensity = ({ region, credits, comparison }) => {
     >
       <VictoryChart
         theme={theme}
-        padding={{ top: 10, bottom: 20, left: 40, right: 20 }}
+        padding={{ top: 10, bottom: 20, left: 30, right: 40 }}
       >
         <VictoryArea
           data={displayData.comparison}
           style={{ data: { fill: '#dadada' } }}
         />
 
+        <VictoryLabel
+          datum={_.last(displayData.comparison)}
+          text={comparison.name}
+          dx={3}
+          dy={3}
+          style={{ fontSize: '7', fontFamily: 'inherit', fill: 'grey' }}
+        />
+
         <VictoryLine data={displayData.region} />
 
-        <VictoryAxis fixLabelOverlap />
+        <VictoryLabel
+          datum={_.last(displayData.region)}
+          text={region.name}
+          dx={3}
+          style={{ fontSize: '7', fontFamily: 'inherit' }}
+        />
+
+        <VictoryAxis fixLabelOverlap tickFormat={val => val.toString()} />
+
         <VictoryAxis
           dependentAxis
           tickFormat={val => Intl.NumberFormat('de').format(val)}
