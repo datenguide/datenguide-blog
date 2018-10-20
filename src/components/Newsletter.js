43556fa1f96f1fa759af65917363bd528621964f
Simon Jockers <hello@simonjockers.de>
2018-10-20 16:20:06 +0200

    Newsletter: Improve presentation on small screens

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -22,24 +22,30 @@ export default function Newsletter(props) {
             Das Neueste vom Datenguide
           </h3>
 
-          <TextField
-            className="newsletter__input"
-            id="email"
-            name="EMAIL"
-            label="Deine E-Mail-Adresse"
-            size="28"
-            withLeadingIcon={<EmailOutlineIcon className="newsletter__icon" />}
-          />
-          <Button className="newsletter__button" outlined type="submit">
-            Newsletter abonnieren
-          </Button>
+          <div className="newsletter__input-group">
+            <TextField
+              className="newsletter__input"
+              id="email"
+              name="EMAIL"
+              label="Deine E-Mail-Adresse"
+              size="32"
+              withLeadingIcon={
+                <EmailOutlineIcon className="newsletter__icon" />
+              }
+            />
+            <Button className="newsletter__button" outlined type="submit">
+              Newsletter abonnieren
+            </Button>
+          </div>
 
           <p className="newsletter__terms">
             Deine Email-Adressen wird von uns ausschliesslich zur Information
             über den Datenguide genutzt und nicht an Dritte weitergegeben. Für
             diesen Verteiler verwenden wir{' '}
             <a href="http://mailchimp.com/">Mailchimp</a>.{' '}
-            <a href="/privacy">Mehr zum Datenschutz bei Datenguide</a>
+            <a href="/privacy" className="newsletter__terms__link">
+              Mehr zum Datenschutz bei Datenguide
+            </a>
           </p>
         </GridCell>
       </Grid>
