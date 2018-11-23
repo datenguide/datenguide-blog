4ceee3b25a63907a65b17db04264c6d792ba41fd
Simon Jockers <hello@simonjockers.de>
2018-11-23 18:28:46 +0100

    :bug: Blog: Fix layout glitch

--- src/components/ArticleHeader.js
+++ src/components/ArticleHeader.js
@@ -5,12 +5,14 @@ import '../scss/components/_article-header.scss'
 
 export default ({ title, description, date, author }) => (
   <div className="article-header">
-    <Grid className="article-header__hgroup">
-      <GridCell span="8">
-        <h1 className="article-header__title">{title}</h1>
-        <p className="article-header__description">{description}</p>
-      </GridCell>
-    </Grid>
+    <div className="article-header__hgroup">
+      <Grid>
+        <GridCell span="8">
+          <h1 className="article-header__title">{title}</h1>
+          <p className="article-header__description">{description}</p>
+        </GridCell>
+      </Grid>
+    </div>
     <Grid className="article-header__meta">
       <GridCell span="8">
         <time className="article-header__date">{date}</time>
