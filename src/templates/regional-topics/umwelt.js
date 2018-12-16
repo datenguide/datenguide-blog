70af953c7f75451a0841aba994710921a59339d4
Simon Jockers <hello@simonjockers.de>
2018-12-16 14:21:46 +0100

    :recycle: Topical pages: Remove unused queries from placeholder pages

--- src/templates/regional-topics/umwelt.js
+++ src/templates/regional-topics/umwelt.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Layout from '../../components/Layout'
 import Header from '../../components/Header'
 import Hero from '../../components/Hero.js'
 import Footer from '../../components/Footer'
 
-export default ({ data, pageContext }) => {
-  const { site } = data
-  const { regionMeta, name, slug } = pageContext
-  const credits = site.siteMetadata.dataCredits
+export default ({ pageContext }) => {
+  const { regionMeta, name } = pageContext
 
   return (
     <Layout>
@@ -21,27 +18,3 @@ export default ({ data, pageContext }) => {
     </Layout>
   )
 }
-
-export const query = graphql`
-  query($id: String!, $comparison: String!) {
-    site {
-      siteMetadata {
-        dataCredits {
-          publisher
-          licenseTitle
-          licenseUrl
-        }
-      }
-    }
-
-    regionDataLegacy: datenguideLegacy {
-      region(id: $id) {
-        ...PopulationDensity
-        ...PopulationDistribution
-      }
-      comparison: region(id: $comparison) {
-        ...PopulationDensity
-      }
-    }
-  }
-`
